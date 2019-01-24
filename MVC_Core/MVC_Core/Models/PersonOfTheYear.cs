7924c88337a809a2918d26de723a9a06e7595bf4
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-23 17:55:08 -0800

    Added all the properties from instructions

--- MVC_Core/MVC_Core/Models/PersonOfTheYear.cs
+++ MVC_Core/MVC_Core/Models/PersonOfTheYear.cs
@@ -7,5 +7,15 @@ namespace MVC_Core.Models
 {
     public class PersonOfTheYear
     {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public int Birth_Year { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+
     }
 }
