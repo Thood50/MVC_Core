54a2884ad4d709ac481f8dbe82fc7bea3beb671e
Tyler Hood <tyler.r.hood@gmail.com>
2019-01-23 18:14:43 -0800

    Alright....wrote out the logic I think thats gonna read in the data from the csv. and populate it into a List of peeps and the did a lamba expression to filter it based on input. I think, its gonna be a minute for me to test this I think.

--- MVC_Core/MVC_Core/Models/PersonOfTheYear.cs
+++ MVC_Core/MVC_Core/Models/PersonOfTheYear.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace MVC_Core.Models
 {
@@ -17,5 +18,32 @@ namespace MVC_Core.Models
         public string Category { get; set; }
         public string Context { get; set; }
 
+        public List<PersonOfTheYear> GetPoTY (int start, int end)
+        {
+            List<PersonOfTheYear> peeps = new List<PersonOfTheYear>();
+            string path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, @"wwwroot\personOfTheYear.csv"));
+            string[] allPoTY = File.ReadAllLines(path);
+
+            for ( int i = 0; i < allPoTY.Length; i++)
+            {
+                peeps.Add(new PersonOfTheYear
+                {
+                    Year = Convert.ToInt32(allPoTY[0]),
+                    Honor = allPoTY[1],
+                    Name = allPoTY[2],
+                    Country = allPoTY[3],
+                    Birth_Year = Convert.ToInt32(allPoTY[4]),
+                    DeathYear = (allPoTY[5] == "") ? 0 :Convert.ToInt32(allPoTY[5]),
+                    Title = allPoTY[6],
+                    Category = allPoTY[7],
+                    Context = allPoTY[8],
+
+                });
+            }
+
+            List<PersonOfTheYear> results = peeps.Where(p => (p.Year >= start) && (p.Year <= end)).ToList();
+
+            return results;
+        }
     }
 }
